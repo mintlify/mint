49c89c91c1439b88c802bd368994787f2fbe757a
Han Wang <hyw2@cornell.edu>
2022-10-20 20:34:03 -0700

    Update arrow on primary cta

--- client/src/ui/Header.tsx
+++ client/src/ui/Header.tsx
@@ -164,18 +164,24 @@ function TopBarCtaButton({ button }: { button: TopbarCta }) {
           target="_blank"
           className={clsx(
             config.classes?.topbarCtaButton ||
-              'relative inline-flex items-center space-x-1.5 px-4 py-1.5 shadow-sm text-sm font-medium rounded-full text-white bg-primary-dark hover:bg-primary-ultradark dark:highlight-white/5'
+              'relative inline-flex items-center space-x-2 px-4 py-1.5 shadow-sm text-sm font-medium rounded-full text-white bg-primary-dark hover:bg-primary-ultradark dark:highlight-white/5'
           )}
         >
           <span>{button.name}</span>
           {!config.classes?.topbarCtaButton && (
             <svg
-              className="h-2 text-white"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
+              width="6"
+              height="3"
+              className="h-2 overflow-visible -rotate-90"
+              aria-hidden="true"
             >
-              <path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" />
+              <path
+                d="M0 0L3 3L6 0"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              />
             </svg>
           )}
         </a>
