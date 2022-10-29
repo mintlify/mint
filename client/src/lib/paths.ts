7e1acc1315dab060cb0fcf32a8ad6880c73c9e44
Han Wang <hyw2@cornell.edu>
2022-10-28 23:03:31 -0700

    Add initial setup for frontend concurrency

--- client/src/lib/paths.ts
+++ client/src/lib/paths.ts
@@ -1,6 +1,47 @@
 import axios from 'axios';
 
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const checkWorkerStatus = async (id: string): Promise<any> => {
+  const status = await axios.get(`${process.env.API_ENDPOINT}/api/v1/admin/build/cache-all/${id}`);
+  return status.data;
+};
+
+const THREE_MINUTES_IN_MS = 1000 * 60 * 3;
+
+export const monitorWorkerStatus = async (id: string) => {
+  let workerStatus = null;
+  let millisecondsPassed = 0;
+  const intervalMs = 100;
+
+  while (workerStatus == null && millisecondsPassed < THREE_MINUTES_IN_MS) {
+    const status = await checkWorkerStatus(id);
+    if (status.state === 'completed' && status.data) {
+      workerStatus = status.data;
+      break;
+    } else if (status.state === 'failed') {
+      throw new Error('Unable to generate documentation');
+    }
+
+    millisecondsPassed += intervalMs;
+    await sleep(intervalMs);
+  }
+
+  return workerStatus;
+};
+
+const cacheAll = async () => {
+  const {
+    data: { id },
+  } = await axios.post(`${process.env.API_ENDPOINT}/api/v1/admin/build/cache-all`);
+  return id;
+};
+
 export const getPaths = async () => {
+  const id = await cacheAll();
+  await monitorWorkerStatus(id);
   const { data }: { data: Record<string, string[][]> } = await axios.get(
     `${process.env.API_ENDPOINT}/api/v1/admin/build/paths`,
     {
