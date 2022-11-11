3c46490762d32e19771f8eeb23f8e20949b7807e
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-11-11 16:31:45 -0500

    Also set basePath

--- client/next.config.js
+++ client/next.config.js
@@ -30,6 +30,7 @@ export default withSentryConfig(
       ],
       disableStaticImages: true,
     },
+    basePath: process.env.NODE_ENV === 'production' ? 'https://mintlify.app' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? 'https://mintlify.app' : '',
     staticPageGenerationTimeout: 1000,
     experimental: {
