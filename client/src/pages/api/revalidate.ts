8ad4027999c4d202caa3d72ab97df8481f069f63
Hahnbee Lee <55263191+hahnbeelee@users.noreply.github.com>
2022-11-11 03:26:42 -0800

    change to urlPath

--- client/src/pages/api/revalidate.ts
+++ client/src/pages/api/revalidate.ts
@@ -1,17 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { secret, path } = req.body;
+  const { secret, urlPath } = req.body;
   if (secret !== process.env.ADMIN_TOKEN) {
     return res.status(401).json({ error: 'Invalid token' });
   }
 
-  if (!path) {
+  if (!urlPath) {
     return res.status(400).json({ error: 'No path provided' });
   }
 
   try {
-    await res.revalidate(path);
+    await res.revalidate(urlPath);
     return res.json({ revalidated: true });
   } catch (err) {
     return res.status(500).send('Error revalidating');
