ef871695b865307cb44eced5188b19e70b388f4d
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-11-28 18:08:30 -0500

    Fix API pages

--- client/src/layouts/ContentSideLayout.tsx
+++ client/src/layouts/ContentSideLayout.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 export function ContentSideLayout({ sticky, children }: { sticky?: boolean; children?: any }) {
   // top-0 would stick to the top of the window. We make it larger to go under the nav bar and have a little padding
   return (
-    <div className={clsx('hidden xl:flex grow', sticky && 'self-start top-20 h-auto sticky')}>
+    <div className={clsx('hidden xl:flex', sticky && 'self-start top-20 h-auto sticky')}>
       {children}
     </div>
   );
