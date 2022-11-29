8de509e2b751b5aaeb35fd99653516b545f5613c
Han Wang <hyw2@cornell.edu>
2022-11-28 17:21:08 -0800

    Add navigation back

--- client/src/ui/MDXContentController/PageHeader.tsx
+++ client/src/ui/MDXContentController/PageHeader.tsx
@@ -34,6 +34,15 @@ export function PageHeader({ title, description, section, isBlogMode }: PageHead
       {description && (
         <p className="mt-2 text-lg text-slate-700 dark:text-slate-400">{description}</p>
       )}
+      <li className="flex items-center font-medium whitespace-nowrap mt-6">
+        <img src="https://mintlify.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdeclan.70da18ee.jpeg&w=96&q=75" alt="" className="mr-3 w-9 h-9 rounded-full bg-slate-50 dark:bg-slate-800 border border-slate-600 dark:border-slate-400" decoding="async" />
+          <div className="text-sm leading-4">
+            <div className="text-slate-900 dark:text-slate-200">Han Wang</div>
+            <div className="mt-1">
+              <a href="https://twitter.com/adamwathan" className="text-primary hover:text-primary-dark dark:text-primary-light">3 min read</a>
+            </div>
+          </div>
+        </li>
     </header>
   );
 }
