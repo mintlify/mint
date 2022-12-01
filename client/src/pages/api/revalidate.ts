3efeeb0fc4b8a1bcd6dd316a0bc8e259bcb17348
Han Wang <hyw2@cornell.edu>
2022-11-30 18:31:48 -0800

    Return revalidate error

--- client/src/pages/api/revalidate.ts
+++ client/src/pages/api/revalidate.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     await res.revalidate(urlPath);
     return res.json({ revalidated: true });
-  } catch (err) {
-    return res.status(500).send('Error revalidating');
+  } catch (error) {
+    return res.status(500).send({ error });
   }
 }
