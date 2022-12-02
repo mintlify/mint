0bb445dec14d127d06d5a181165d35786a25a3dc
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-12-02 15:51:57 -0500

    Make revalidate paths fully async

--- client/src/pages/api/revalidate-paths.ts
+++ client/src/pages/api/revalidate-paths.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { secret, paths }: { secret: string; paths: string[] } = req.body;
   if (secret !== process.env.ADMIN_TOKEN) {
     return res.status(401).json({ error: 'Invalid token' });
@@ -8,17 +8,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (!paths) {
     return res.status(400).json({ error: 'No paths provided' });
   }
-  const promises: Promise<string>[] = paths.map(
-    (path) =>
-      new Promise(async (resolve, reject) => {
-        try {
-          await res.revalidate(path);
-          return resolve(path);
-        } catch (err: any) {
-          return reject(path);
-        }
-      })
-  );
-  const response = await Promise.allSettled(promises);
-  return res.json(response);
+
+  paths.forEach((path) => {
+    res.revalidate(path);
+  });
+
+  // 202 because we guarantee we started the async process, but do not know if it worked
+  return res.status(202);
 }
