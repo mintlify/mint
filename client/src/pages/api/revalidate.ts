804541047cb619555c8f4939d4f261694f749f6f
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-12-06 10:02:17 -0500

    Improve output format

--- client/src/pages/api/revalidate.ts
+++ client/src/pages/api/revalidate.ts
@@ -3,17 +3,17 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { secret, urlPath } = req.body;
   if (secret !== process.env.ADMIN_TOKEN) {
-    return res.status(401).json({ error: 'Invalid token' });
+    return res.status(401).json({ revalidated: false, error: 'Invalid token' });
   }
 
   if (!urlPath) {
-    return res.status(400).json({ error: 'No path provided' });
+    return res.status(400).json({ revalidated: false, error: 'No path provided' });
   }
 
   try {
     await res.revalidate(urlPath);
     return res.json({ revalidated: true });
   } catch (error) {
-    return res.status(500).send({ error });
+    return res.status(500).json({ revalidated: false, error });
   }
 }
