5d2b7d3a08f39e1e47922936afcb28ecbc75e53c
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-12-14 23:46:16 -0500

    Import Fira Code

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,19 +1,22 @@
-import { Inter } from '@next/font/google';
+import { Inter, Fira_Code } from '@next/font/google';
 import 'focus-visible';
 
 import ErrorBoundary from '@/ui/ErrorBoundary';
 
-import '../css/fonts.css';
 import '../css/main.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const firaCode = Fira_Code({
+  subsets: ['latin'],
+  variable: '--font-fira-code',
+});
 
 export default function App(props: any) {
   const { Component, pageProps } = props;
 
   return (
     <ErrorBoundary>
-      <main className={inter.className}>
+      <main className={`${inter.variable} ${firaCode.variable} font-sans`}>
         <Component {...pageProps} />
       </main>
     </ErrorBoundary>
