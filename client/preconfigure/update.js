934037b8ccadf6a6c13a6bfa6a6fbd7a4d045cfc
Hahnbee Lee <55263191+hahnbeelee@users.noreply.github.com>
2022-12-23 09:22:28 -0800

    generateNav and favicons

--- client/preconfigure/update.js
+++ client/preconfigure/update.js
@@ -1,6 +1,9 @@
 import { promises as _promises } from 'fs';
-import fse, { pathExists } from 'fs-extra';
+import fse, { pathExists, outputFile } from 'fs-extra';
 import path from 'path';
+
+import createPage from './createPage';
+import { generateFavicons, generateNavFromPages } from './generate';
 
 const { readFile } = _promises;
 
@@ -66,7 +69,37 @@ export const updateStaticFiles = (contentDirectoryPath, staticFilenames) => {
   return staticFilePromises;
 };
 
-export const updateContentFiles = async (contentDirectoryPath, contentFiles) => {};
+export const updateFavicons = async (mintConfig, contentDirectoryPath) => {
+  const generatedFavicons = await generateFavicons(mintConfig, contentDirectoryPath);
+  const promises = [];
+  generatedFavicons.images.forEach((img) => {
+    promises.push(
+      (async () => {
+        const targetPath = path.join('public', 'favicons', img.name);
+        await outputFile(targetPath, Buffer.from(img.contents), {
+          flag: 'w',
+        });
+      })()
+    );
+  });
+  generatedFavicons.files.forEach((file) => {
+    promises.push(
+      (async () => {
+        const targetPath = path.join('public', 'favicons', file.name);
+        await outputFile(targetPath, file.contents, { flag: 'w' });
+      })()
+    );
+  });
+  await Promise.all(promises);
+};
+
+export const updateGeneratedNav = async (pages, mintConfigNav) => {
+  const generatedNav = await generateNavFromPages(pages, mintConfigNav);
+  const targetPath = path.join('src', '_props', 'generatedNav.json');
+  await outputFile(targetPath, JSON.stringify(generatedNav, null, 2), {
+    flag: 'w',
+  });
+};
 
 export const update = async (
   contentDirectoryPath,
@@ -74,7 +107,7 @@ export const update = async (
   openApiFiles,
   contentFilenames
 ) => {
-  let pages = {};
+  let pagesAcc = {};
   const contentPromises = [];
   contentFilenames.forEach((filename) => {
     contentPromises.push(
@@ -87,18 +120,22 @@ export const update = async (
 
         const fileContent = await readFile(sourcePath);
         const contentStr = fileContent.toString();
-        const page = createPage(filename, contentStr, openApiFiles);
-        pages = {
-          ...pages,
+        const page = createPage(filename, contentStr, contentDirectoryPath, openApiFiles);
+        pagesAcc = {
+          ...pagesAcc,
           ...page,
         };
       })()
     );
   });
-  const response = await Promise.all([
+  const initialFileUploadResponses = await Promise.all([
     updateConfigFile(contentDirectoryPath),
+    ...contentPromises,
     ...updateStaticFiles(contentDirectoryPath, staticFilenames),
     updateOpenApiFiles(openApiFiles),
   ]);
-  return response[0];
+  const config = initialFileUploadResponses[0];
+  await generateNavFromPages(pagesAcc, config?.navigation);
+  // generateFavicon(config);
+  return config;
 };
